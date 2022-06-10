234d23d09d25adbe00f95912b51dd443b7d96b7f
Павел Солдатов <pavlik.soldatov.12@gmail.com>
2022-06-10 06:31:38 +0400

    1

--- app.js
+++ app.js
@@ -34,61 +34,75 @@ app.use(
 )
 
 app.get("/auth", cors(), (req, res) => {
-    if(!req.session.key) {
-        req.session.key = req.sessionID
-    }
+    if(!req.session.key) req.session.key = req.sessionID
 
-    if(!req?.query?.endpoint && !req?.query?.method) {
-        if(!req.query?.data) {
-            req.session.key[req.sessionID] = {
-                did: "",
-                authorized: false,
-                value: auth.generateMessage(),
-                endpoint: req.query.endpoint,
-                data: req.query.data
-            }
-        } else {
-            req.session.key[req.sessionID] = {
-                did: "",
-                authorized: false,
-                value: auth.generateMessage(),
-                endpoint: req.query.endpoint
-            }
-        }
+    if(req?.query?.endpoint && req?.query?.method) {
+        req.session.message = auth.generateMessage()
+        req.session.endpoint = req.query.endpoint
+        req.session.method = req.query.method
+        if(req.query?.data) req.session.data = req.query.data
+        
         var dataQr = {
             endpoint: `http://${host}:${port}/response`,
             method: "post",
-            message: req.session.key[req.sessionID].value,
+            message: req.session.message,
             sessionToken: req.sessionID
+        }
+        console.log(dataQr)
+        res.sendStatus(302)
+    } else {
+        res.sendStatus(400)
+    }
+})
+
+
+app.get("/", cors(), (req, res) => {
+    // if(!req.session.key) req.session.key = req.query.sessionID
+    res.send(req.session[req.query.sessionID])
+})
+
+app.get("/request", cors(), (req, res) => {
+    if(!req.session.key) {
+        res.sendStatus(403)
+    } else {
+        if(req.session.key[req.sessionID].authorized) {
+            // res.send(JSON.stringify({
+            //     did: req.session.key[req.sessionID],
+            //     authorized: req.session.key[req.sessionID]
+            // }))
+            res.json({
+                did: req.session.key[req.sessionID],
+                authorized: req.session.key[req.sessionID]
+            })
+            res.redirect(req.session.key[req.sessionID].endpoint)
+        } else {
+            res.sendStatus(102)
+        }
+    }
+})
+
+
+// app.post("/auth/login", cors(), (req, res) => {
+    
+// })
+
+app.post("/response", cors(), async (req, res) => {
+    //Тут должна быть проверка что запрос прислало именно наше приложение
+    if(req.body?.did && req.body?.parameter && req.body?.sessionToken) {
+        var check = auth.signin(req.body.did, req.session.key[req.body.sessionToken], req.body.parameter)
+        if(check) {
+            req.session.key[req.body.sessionToken].did = req.body.did,
+            req.session.key[req.body.sessionToken].authorized = true
+        } else {
+            res.sendStatus(401)
         }
     } else {
         res.sendStatus(400)
     }
 
-
-})
-
-app.get("/request", cors(), (req, res) => {
-     if(req.body?.did && req.body?.parameters && req.body?.sessionToken) {
-         //Code
-     } else {
-         res.sendStatus(404);
-     }
-})
-
-app.post("/auth/login", cors(), (req, res) => {
     
-})
 
-app.post("/response", cors(), async (req, res) => {
-    //Тут должна быть проверка что запрос прислало именно наше приложение
-
-    var check = auth.signin(req.body.did, req.session.key[req.body.sessionToken], req.body.parameter)
-
-    if(check) {
-        req.session.key[req.body.sessionToken].did = req.body.did,
-        req.session.key[req.body.sessionToken].authorized = true
-    }
+    
 })
 
 app.listen(port, host, async () => {
